754b43656e0c17d02a1e90964651f821a2ac8328
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-25 13:00:24 -0400

    Make map fit full screen and work.

--- guess/src/App.tsx
+++ guess/src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { client_id } from "./constants"
 
 const App: React.FC = () => {
+
+  const [dimensions, setDimensions] = useState([0, 0]);
+
+  useEffect(() => {
+    setDimensions([window.innerWidth, window.innerHeight]);
+
+    setTimeout(() => {
+      const viewer = new (window as any).Mapillary.Viewer(
+        "map",
+        client_id,
+        "Z2HdXiZS7l0rYnjahLWo4Q"
+      );
+      
+      console.log(viewer);
+    }, 100)
+  }, []);
+
   return (
     <>
+      {
+        dimensions[0] != 0 ?
+        <div id="map" style={{width: "100%", height: dimensions[1]}}></div>
+        :
+        <h1>
+          Loading
+        </h1>
+      }
     </>
   );
 }
---
+++ guess/src/constants.ts
@@ -0,0 +1,5 @@
+const client_id = "NXVGMWlCc2ozUmQ5a05DQzdiR1lKVTowZTcyMWU5ZTBlMzQzOGE2"
+
+export {
+    client_id
+}
