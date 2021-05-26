7d93642800aa6ab8f93dbcf4b8344741533276e3
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-25 21:26:04 -0400

    Attempt to draw line between guess and answer

--- guess/src/components/DistanceMap.tsx
+++ guess/src/components/DistanceMap.tsx
@@ -21,6 +21,36 @@ const DistanceMap: React.FC<{guess: number[], answer: number[]}> = ({guess, answ
             color: "#ff5842"
         }).setLngLat(answer).addTo(map);
 
+        // map.on("load", () => {
+        //     map.addSource("line", {
+        //         type: "geojson",
+        //         data: {
+        //             type: "Feature",
+        //             properties: {},
+        //             geometry: {
+        //                 type: "LineString",
+        //                 coordinates: [guess, answer]
+        //             }
+        //         }
+        //     });
+        // });
+
+        // map.addLayer({
+        //     id: "line",
+        //     type: "line",
+        //     source: "line",
+        //     layout: {
+        //         'line-join': 'round',
+        //         'line-cap': 'round'
+        //     },
+        //     paint: {
+        //         'line-color': '#888',
+        //         'line-width': 8
+        //     }
+        // })
+
+        setTimeout(() => map.zoomOut(), 250);
+
     }, [])
 
     return (
