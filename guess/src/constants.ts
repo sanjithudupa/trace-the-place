d8cf7387db6d68441bc838a49264848f8cd4704e
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-28 08:07:45 -0400

    rm constants

--- guess/src/constants.ts
+++
@@ -1,7 +0,0 @@
-const client_id = "NXVGMWlCc2ozUmQ5a05DQzdiR1lKVTowZTcyMWU5ZTBlMzQzOGE2"
-const mapbox_access_token = "pk.eyJ1Ijoic2Fuaml0aHVkdXBhIiwiYSI6ImNrZ3kxZGtuMzBwamUycnF6em1xdGZhYzkifQ.o5_lbTkiChpXgwXnsygd9Q";
-
-export {
-    client_id,
-    mapbox_access_token
-}
