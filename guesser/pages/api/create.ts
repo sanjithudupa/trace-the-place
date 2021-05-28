bb6e324557320dbe3d76cf9ad94b89f08b3e9349
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-05-28 08:05:27 -0400

    Remove old next project

--- guesser/pages/api/create.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import places from "../../util/places.json";
-
-const place_array = places.places;
-
-export default (_: NextApiRequest, res: NextApiResponse) => {
-    const place = place_array[Math.floor(Math.random()*place_array.length)];
-    res.status(200).json({ id: place.id });
-}
